6d41285b70a87c24dc1a8b6a37d01d0dea15581e
Wen Chen <wen.chen97@gmail.com>
2021-02-16 23:39:58 +0000

    Intro to string manipulation

---
+++ Regular Expression.py
@@ -0,0 +1,83 @@
+# Find characters in movie variable
+length_string = print(len(movie))
+
+# Get the word
+movie_title = movie[11:30]
+
+# Obtain the palindrome
+palindrome = movie_title[::-1]
+
+# Print the word if it's a palindrome
+if movie_title == palindrome:
+	print(movie_title)
+
+# Convert to lowercase and print the result
+movie_lower = movie.lower()
+print(movie_lower)
+
+# Remove specified character and print the result
+movie_no_sign = movie.strip("$")
+print(movie_no_sign)
+
+
+# Split the string into substrings and print the result
+movie_split = movie_no_sign.split()
+print(movie_split)
+
+# Select root word and print the result
+word_root = movie_split[1][:-1]
+print(word_root)
+
+# Remove tags happening at the end and print results
+movie_tag = movie.rstrip("<\i>")
+print(movie_tag)
+
+# Split the string using commas and print results
+movie_no_comma = movie_tag.split(",")
+print(movie_no_comma)
+
+# Join back together and print results
+movie_join = " ".join(movie_no_comma)
+print(movie_join)
+
+# Split string at line boundaries
+file_split = file.splitlines()
+
+# Print file_split
+print(file_split)
+
+# Complete for-loop to split by commas
+for substring in file_split:
+    substring_split = substring.split(sep=',',)
+    print(substring_split)
+
+for movie in movies:
+  	# Find if actor occurrs between 37 and 41 inclusive
+    if movie.find("actor", 37, 42) == -1:
+        print("Word not found")
+    # Count occurrences and replace two by one
+    elif movie.count("actor") == 2:
+        print(movie.replace("actor actor", "actor"))
+    else:
+        # Replace three occurrences by one
+        print(movie.replace("actor actor actor", "actor"))
+
+for movie in movies:
+  # Find the first occurrence of word
+  print(movie.find("money", 12, 51))
+
+for movie in movies:
+  try:
+    # Find the first occurrence of word
+  	print(movie.index("money", 12, 51))
+  except ValueError:
+    print("substring not found")
+
+# Replace negations
+movies_no_negation = movies.replace("isn't", "is")
+
+# Replace important
+movies_antonym = movies_no_negation.replace("important", "insignificant")
+
+# Print out
+print(movies_antonym)
