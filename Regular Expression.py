43b585a1d5ec66f0062a9787090a00337be2f21c
Wen Chen <wen.chen97@gmail.com>
2021-02-21 17:29:21 +0000

    Regular expression 2

--- Regular Expression.py
+++ Regular Expression.py
@@ -264,6 +264,16 @@ numbers_found_greedy = re.findall(r"\d+?", sentiment_analysis)
 # Print out the result
 print(numbers_found_greedy)
 
+# Write a regex that matches email
+regex_email = r"([A-Za-z]+)([0-9]+)(\S+)@"
+
+for tweet in sentiment_analysis:
+    # Find all matches of regex in each tweet
+    email_matched = re.findall(regex_email, tweet)
+
+    # Complete the format method to print the results
+    print("Lists of users found in this tweet: {}".format(tweet))
+
 # Write a greedy regex expression to match
 sentences_found_greedy = re.findall(r"\(.+\)", sentiment_analysis)
 
@@ -275,3 +285,95 @@ sentences_found_lazy = re.findall(r"\(.+?\)", sentiment_analysis)
 
 # Print out the results
 print(sentences_found_lazy)
+
+# Import re
+import re
+
+# Write regex to capture information of the flight
+regex = r"([A-Z]{2})(\d{4})\s([A-Z]{3})-([A-Z]{3})\s(\d{2}[A-Z]{3})"
+
+# Find all matches of the flight information
+flight_matches = re.findall(regex, flight)
+
+# Import re
+import re
+
+# Write regex to capture information of the flight
+regex = r"([A-Z]{2})(\d{4})\s([A-Z]{3})-([A-Z]{3})\s(\d{2}[A-Z]{3})"
+
+# Find all matches of the flight information
+flight_matches = re.findall(regex, flight)
+
+# Print the matches
+print("Airline: {} Flight number: {}".format(flight_matches[0][0], flight_matches[0][1]))
+print("Departure: {} Destination: {}".format(flight_matches[0][2], flight_matches[0][3]))
+print("Date: {}".format(flight_matches[0][4]))
+
+# Write a regex that matches sentences with the optional words
+regex_positive = r"(love|like|enjoy).+?(movie|concert)\s(.+?)\."
+
+for tweet in sentiment_analysis:
+    # Find all matches of regex in tweet
+    positive_matches = re.findall(regex_positive, tweet)
+
+    # Complete format to print out the results
+    print("Positive comments found {}".format(positive_matches))
+
+# Write regex and scan contract to capture the dates described
+regex_dates = r"Signed\son\s(\d{2})/(\d{2})/(\d{4})"
+dates = re.search(regex_dates, contract)
+
+# Assign to each key the corresponding match
+signature = {
+	"day": dates.group(2),
+	"month": dates.group(1),
+	"year": dates.group(3)
+}
+
+# Write regex and scan contract to capture the dates described
+regex_dates = r"Signed\son\s(\d{2})/(\d{2})/(\d{4})"
+dates = re.search(regex_dates, contract)
+
+# Assign to each key the corresponding match
+signature = {
+	"day": dates.group(2),
+	"month": dates.group(1),
+	"year": dates.group(3)
+}
+# Complete the format method to print-out
+print("Our first contract is dated back to {data[year]}. Particularly, the day {data[day]} of the month {data[month]}.".format(data=signature))
+
+for string in html_tags:
+    # Complete the regex and find if it matches a closed HTML tags
+    match_tag = re.match(r"<(\w+)>.*?</\1>", html_tags)
+
+    if match_tag:
+        # If it matches print the first group capture
+        print("Your tag {} is closed".format(match_tag.group(1)))
+    else:
+        # If it doesn't match capture only the tag
+        notmatch_tag = re.match(r"<(\w+)>", match_tag)
+        # Print the first group capture
+        print("Close your {} tag!".format(notmatch_tag.group(1)))
+
+# Complete the regex to match an elongated word
+regex_elongated = r"\w+(\w+)\1\w*"
+regex_elongated = r"\w*(\w)\1\w*"
+
+for tweet in sentiment_analysis:
+    # Find if there is a match in each tweet
+    match_elongated = re.search(regex_elongated, tweet)
+
+    if match_elongated:
+        # Assign the captured group zero
+        elongated_word = match_elongated.group(0)
+
+        # Complete the format method to print the word
+        print("Elongated word found: {word}".format(word=regex_elongated))
+    else:
+        print("No elongated word found")
+
+for phone in cellphones:
+	# Get all phone numbers not preceded by area code
+	number = re.findall(r"(?<!\d{3}-)\d{4}-\d{6}-\d{2}", phone)
+	print(number)
