ef6e7d72e56b395240b9ea593acb4201c2bd7c49
Wen Chen <wen.chen97@gmail.com>
2021-02-21 00:06:58 +0000

    Regular expression

--- Regular Expression.py
+++ Regular Expression.py
@@ -175,3 +175,103 @@ try:
     print(the_answers.safe_substitute(answers))
 except KeyError:
     print("Missing information")
+
+# Write a regex to obtain user mentions
+print(re.findall(r"User_mentions:\d", sentiment_analysis))
+
+# Write a regex to obtain number of likes
+print(re.findall(r"likes:\s\d", sentiment_analysis))
+print(re.findall(r"number\sof\sretweets:\s\d", sentiment_analysis))
+
+regex_sentence = r"\W\dbreak\W"
+# Replace the regex_sentence with a space
+sentiment_sub = re.sub(regex_sentence, " ", sentiment_analysis)
+# Write a regex to match pattern separating words
+regex_words = r"\Wnew\w"
+# Replace the regex_words and print the result
+sentiment_final = re.sub(regex_words, " ", sentiment_sub)
+print(sentiment_final)
+
+# Import re module
+import re
+
+for tweet in sentiment_analysis:
+	# Write regex to match http links and print out result
+	print(re.findall(r"http\S+", tweet))
+
+	# Write regex to match user mentions and print out result
+	print(re.findall(r"@\w+", tweet))
+
+# Complete the for loop with a regex to find dates
+for date in sentiment_analysis:
+	print(re.findall(r"\d{1,2}\s\w+\sago", date))
+
+# Complete the for loop with a regex to find dates
+for date in sentiment_analysis:
+	print(re.findall(r"\d{1:2}\w{2}\s\w+\s\d{4}", date))
+
+	print(re.findall(r"\d{1,2}\w{2}\s\w+\s\d{4}\s\d{1,2}:\d{2}", date))
+
+# Write a regex matching the hashtag pattern
+regex = r"#\w+"
+
+# Replace the regex by an empty string
+no_hashtag = re.sub(regex, "", sentiment_analysis)
+
+# Get tokens by splitting text
+print(re.split(r"\s+",no_hashtag ))
+
+egex = r"[a-zA-Z]+\d+\W+@\w+\.com"
+
+
+# Write a regex to match a valid email address
+regex = r"[a-zA-Z]+[0-9]+[!#\%&*$\.]+@\w+\.com"
+
+for example in emails:
+  	# Match the regex to the string
+    if re.match(regex, emails):
+        # Complete the format method to print out the result
+      	print("The email {email_example} is a valid email".format(email_example=example))
+    else:
+      	print("The email {email_example} is invalid".format(email_example=example))
+
+
+# Write a regex to match a valid password
+regex = r"[a-zA-Z0-9*#\$%!&\.]{8:20}"
+        r"[A-Za-z0-9!#%&*\$\.]{8,20}"
+
+for example in passwords:
+  	# Scan the strings to find a match
+    if re.match(regex, passwords):
+        # Complete the format method to print out the result
+      	print("The password {pass_example} is a valid password".format(pass_example=example))
+    else:
+      	print("The password {pass_example} is invalid".format(pass_example=example))
+
+
+# Import re
+import re
+
+# Write a regex to eliminate tags
+string_notags = re.sub(r"<.+?>", " ", string)
+
+# Print out the result
+print(string_notags)
+
+# Write a greedy regex expression
+numbers_found_greedy = re.findall(r"\d+?", sentiment_analysis)
+
+# Print out the result
+print(numbers_found_greedy)
+
+# Write a greedy regex expression to match
+sentences_found_greedy = re.findall(r"\(.+\)", sentiment_analysis)
+
+# Print out the result
+print(sentences_found_greedy)
+
+# Write a lazy regex expression
+sentences_found_lazy = re.findall(r"\(.+?\)", sentiment_analysis)
+
+# Print out the results
+print(sentences_found_lazy)
